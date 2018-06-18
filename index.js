a426e83f945d6bdcbce2220b8a2ef96046ffd3fa
Marco Solazzi <marco.solazzi@gmail.com>
2018-06-18 15:34:19 +0900

    refactor: allow a server instance as argument
    allows both configuration object and server instance as first argument

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const { Server } = require('./lib/server');
 const connect = require('./lib/connect');
 const koa = require('./lib/koa');
+const { createWatcher } = require('./lib/watcher');
 const { transformJSON, transformText } = require('./lib/utils');
 
-const applyMiddleware = (middleware) => (options) => middleware(new Server(options));
+const applyMiddleware = (middleware) => (arg) => {
+  const srv = arg instanceof Server ? arg : new Server(arg);
+  return middleware(srv);
+};
 
 module.exports = {
   Server,
   transformJSON,
   transformText,
+  createWatcher,
   applyMiddleware,
   koaServer: applyMiddleware(koa),
   connectServer: applyMiddleware(connect)
