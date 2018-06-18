0aa06a3bf094006bd904fef95cc046cf87e163aa
Marco Solazzi <marco.solazzi@gmail.com>
2018-06-18 11:56:34 +0900

    refactor: saner middleware registration logic

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { Server } = require('./lib/server');
 const connect = require('./lib/connect');
+const koa = require('./lib/koa');
 const { transformJSON, transformText } = require('./lib/utils');
+
+const applyMiddleware = (middleware) => (options) => middleware(new Server(options));
 
 module.exports = {
   Server,
   transformJSON,
   transformText,
-  connectServer: (options) => connect(new Server(options))
+  applyMiddleware,
+  koaServer: applyMiddleware(koa),
+  connectServer: applyMiddleware(connect)
 }
